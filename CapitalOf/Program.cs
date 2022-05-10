53a7ce80249beb43d1692063bfa47936ae192b28
Yulia Gvardeeva <julia.athen@yandex.ru>
2022-05-10 18:58:50 +0300

    add task with format

---
+++ CapitalOf/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите: <государство> <столица>");
+var input = Console.ReadLine(); 
+var splitted = input.Split(' ');    
+
+Console.WriteLine($"Столица государства {splitted[0]} - город {splitted[1]}");
+Console.WriteLine(String.Format("Столица государства {0} - город {1}",splitted[0], splitted[1]));
+
